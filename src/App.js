16e66128420fb60ddce9d6c8ee38dfa846bd2de4
Alexandre Kelifa <alexandre.kelifa@hotmail.com>
2022-10-07 14:27:54 +0200

    Created an array of object with one property img; Created a function to shuffle them by duplicateting the array twice, then sort it out with a random function, then map through it adding IDs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
 import './App.css';
 
+const cardImages = [
+  {"src" : "public/img/bisouVolant.webp"},
+  {"src" : "public/img/etBim.webp"},
+  {"src" : "public/img/genie.webp"},
+  {"src" : "public/img/onFire.webp"},
+  {"src" : "public/img/runPizza.webp"},
+  {"src" : "public/img/star.webp"}
+]
+
 function App() {
+
+  // 1-Duplicate each card once = 12 cards
+  // 2-Randomize the order of the cards 
+  // 3-Give them a kez
+
+  const shuffleCards = () => {
+    // Duplicate the array once ... and twice
+    const shuffleCards = [...cardImages, ...cardImages]
+    // Mix images with the sort() method
+    .sort(() => Math.random()-0.5)
+    .map((card ) => ({...card, id: Math.random()}))
+
+  }
+
   return (
     <div className="App">
       <h1>MEMORY GAME</h1>
