106db73951cf5d0d3964a8cda1b2dcba7fd57b90
Alexandre Kelifa <alexandre.kelifa@hotmail.com>
2022-10-07 14:36:11 +0200

    Created Card state and turns state

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 
 const cardImages = [
@@ -11,6 +12,11 @@ const cardImages = [
 
 function App() {
 
+  // Create a state with an emty array by default
+  const [cards, setCards] = useState([])
+  // Create a state for the number of turns
+  const [turns, setTurns] = useState(0)
+
   // 1-Duplicate each card once = 12 cards
   // 2-Randomize the order of the cards 
   // 3-Give them a kez
@@ -21,6 +27,11 @@ function App() {
     // Mix images with the sort() method
     .sort(() => Math.random()-0.5)
     .map((card ) => ({...card, id: Math.random()}))
+
+    // set a new state with the randomized cards array
+    setCards(shuffleCards)
+    // Reset turns to 0 for a new Game
+    setTurns(0)
 
   }
 
